832745ba06948e2e3704896bcede2bdb207beb55
Simplation <simplationwang@gmail.com>
2021-07-15 18:04:04 +0800

    Create BannerResponse.kt

---
+++ jetpackmvvm/src/main/java/com/simplation/mvvm/data/model/bean/BannerResponse.kt
@@ -0,0 +1,32 @@
+package com.simplation.mvvm.data.model.bean
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+/**
+ * 轮播图
+ * Banner response
+ *
+ * @property desc
+ * @property id
+ * @property imagePath
+ * @property isVisible
+ * @property order
+ * @property title
+ * @property type
+ * @property url
+ * @constructor Create empty Banner response
+ */
+@Parcelize
+data class BannerResponse(
+    var desc: String = "",
+    var id: Int = 0,
+    var imagePath: String = "",
+    var isVisible: Int = 0,
+    var order: Int = 0,
+    var title: String = "",
+    var type: Int = 0,
+    var url: String = ""
+) : Parcelable
+
+
